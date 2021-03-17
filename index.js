e9e8b39fc12b5b1c0e72d6b2ab095b2093c79c5b
Romario Gustave <romagustave10@gmail.com>
2021-03-17 10:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+class Polygon {
+  constructor(count, perimeter) {
+    this.count = count;
+    this.perimeter = perimeter;
+  }
+
+class Square extends Polygon{
+  // inherits constructor from Polygon
+}
