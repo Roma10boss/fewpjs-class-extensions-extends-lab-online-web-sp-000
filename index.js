8f2f163520a3779a713c6b87baa18c897a1721c5
Romario Gustave <romagustave10@gmail.com>
2021-03-17 10:37:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 // Your code here
 class Polygon {
-  constructor(count, perimeter) {
-    this.count = count;
-    this.perimeter = perimeter;
-  }
+    constructor(sidesArr) {
+        this.sidesArr = sidesArr
+    }
 
-class Square extends Polygon{
-  // inherits constructor from Polygon
+    get countSides() {
+        return this.sidesArr.length
+    }
+
+    get perimeter() {
+        return this.sidesArr.reduce((a, b) => a + b, 0)
+    }
+}
+
+
+class Triangle extends Polygon {
+    
+    get isValid() {
+      let sides = this.sidesArr.sort()
+      return (sides[2] < (sides[0] + sides[1]))
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let sides = new Set(this.sidesArr)
+        return (sides.size === 1)
+    }
+
+    get area() {
+        return (this.sidesArr[0] ** 2)
+    }
+  
 }
